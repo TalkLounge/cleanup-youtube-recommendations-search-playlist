a9225ca7719428af3213d0c1afd83cb9d3c31ea9
TalkLounge <talklounge@yahoo.de>
2021-12-05 13:19:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Cleanup YouTube Search
-// @version      1.0
+// @version      1.0.1
 // @description  Deletes "Users have also seen", "For you" and "Similar searches" Sections from Youtube Search Results
 // @author       TalkLounge (https://github.com/TalkLounge)
 // @namespace    https://github.com/TalkLounge/cleanup-youtube-search
 // @license      MIT
-// @match        https://www.youtube.com/results*
+// @match        https://www.youtube.com*
 // @grant        none
 // ==/UserScript==
 
@@ -13,6 +13,10 @@
     'use strict';
 
     function cleanup() {
+        if (! window.location.href.startsWith("https://www.youtube.com/results")) {
+            return;
+        }
+
         var shelfs = document.getElementsByTagName("ytd-shelf-renderer");
         for (var i = 0; i < shelfs.length; i++) {
             shelfs[i].remove();
