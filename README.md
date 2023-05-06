b32aefce4fd65cb6c4135e24c5a82c5a09e9dc04
TalkLounge <talklounge@yahoo.de>
2023-05-06 14:16:46 +0200

    Recommendations are now also removed from playlists

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Cleanup YouTube Search
-Tampermonkey Userscript Deleting "Users have also seen", "For you" and "Similar searches" Sections from Youtube Search Results
+# Cleanup YouTube Recommendations in Search and Playlist
+Tampermonkey Userscript Deleting "Users have also seen", "For you", "Similar searches" and "Recommended videos" Sections from Search Results and Playlists on YouTube
 
 [Install on Greasyfork](https://greasyfork.org/scripts/436569)
 
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name            Cleanup YouTube Search
-// @name:de         YouTube Suche bereinigen
-// @version         1.0.2
-// @description     Deletes "Users have also seen", "For you" and "Similar searches" Sections from Youtube Search Results
-// @description:de  Entfernt die Abschnitte "Nutzer haben auch gesehen", "Für mich" und "Ähnliche Suchanfragen" aus den Youtube-Suchergebnissen
+// @name            Cleanup YouTube Recommendations in Search and Playlist
+// @name:de         Bereinige YouTube Empfehlungen in der Suche und bei Playlisten
+// @version         1.1.2
+// @description     Deletes "Users have also seen", "For you", "Similar searches" and "Recommended videos" Sections from Search Results and Playlists on YouTube
+// @description:de  Entfernt die Abschnitte "Nutzer haben auch gesehen", "Für mich", "Ähnliche Suchanfragen" und "Empfohlene Videos" aus den Suchergebnissen und aus Playlisten auf YouTube
+// @icon            https://www.google.com/s2/favicons?domain=youtube.com
 // @author          TalkLounge (https://github.com/TalkLounge)
-// @namespace       https://github.com/TalkLounge/cleanup-youtube-search
+// @namespace       https://github.com/TalkLounge/cleanup-youtube-recommendations-search-playlist
 // @license         MIT
 // @match           https://www.youtube.com/*
 // @grant           none
@@ -15,23 +16,29 @@
     'use strict';
 
     function cleanup() {
-        if (! window.location.href.startsWith("https://www.youtube.com/results")) {
-            return;
+        if (window.location.href.startsWith("https://www.youtube.com/playlist")) {
+            try {
+                document.querySelector('ytd-item-section-renderer[page-subtype="playlist"]:nth-child(2)').remove();
+            } catch (e) {}
+
+            try {
+                document.querySelector('ytd-item-section-renderer[page-subtype="playlist"]:nth-child(2)').remove();
+            } catch (e) {}
         }
 
-        var shelfs = document.getElementsByTagName("ytd-shelf-renderer");
-        for (var i = 0; i < shelfs.length; i++) {
-            shelfs[i].remove();
-        }
+        if (window.location.href.startsWith("https://www.youtube.com/results")) {
+            let shelfs = document.getElementsByTagName("ytd-shelf-renderer");
+            for (let i = 0; i < shelfs.length; i++) {
+                shelfs[i].remove();
+            }
 
-        shelfs = document.getElementsByTagName("ytd-horizontal-card-list-renderer");
-        for (var i = 0; i < shelfs.length; i++) {
-            shelfs[i].remove();
+            shelfs = document.getElementsByTagName("ytd-horizontal-card-list-renderer");
+            for (let i = 0; i < shelfs.length; i++) {
+                shelfs[i].remove();
+            }
         }
     }
 
     cleanup();
-    setInterval(function () {
-        cleanup();
-    }, 1000);
+    setInterval(cleanup, 1000);
 })();
