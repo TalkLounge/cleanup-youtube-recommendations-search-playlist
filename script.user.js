b8a950b60aded97dbc28aad5c65f3d5a46d7f00b
TalkLounge <talklounge@yahoo.de>
2024-05-02 22:39:20 +0200

    Update to 1.1.6: Block new recommended playlist section && Adjust Year in LICENSE

--- script.user.js
+++ script.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name            Cleanup YouTube Recommendations in Search and Playlist
 // @name:de         Bereinige YouTube Empfehlungen in der Suche und bei Playlisten
-// @version         1.1.5
-// @description     Deletes "Popular videos from today", "Users have also seen", "New channels for you", "For you", "Already viewed", "Recommended playlists" and "Recommended videos" Sections from Search Results and Playlists on YouTube
-// @description:de  Entfernt die Abschnitte "Beliebte Videos von heute", Nutzer haben auch gesehen", "Neue Kanäle für dich", "Für mich", "Schon angesehen", "Empfohlene Playlists" und "Empfohlene Videos" aus den Suchergebnissen und aus Playlisten auf YouTube
+// @version         1.1.6
+// @description     Deletes "People also watched", "For you", "Previously watched", "From related searches", "Latest from", "Popular videos from today", "New channels for you", "People also search for", "Recommended playlists" and "Recommended videos" Sections from Search Results, Playlists and Channels on YouTube
+// @description:de  Entfernt die Abschnitte "Nutzer haben auch gesehen", "Für mich", "Schon angesehen", "Aus ähnlichen Suchanfragen", "Neueste Videos von", "Beliebte Videos von heute", "Neue Kanäle für dich", "Ähnliche Suchanfragen", "Empfohlene Playlists" und "Empfohlene Videos" aus den Suchergebnissen, Playlisten und Kanälen auf YouTube
 // @icon            https://www.google.com/s2/favicons?domain=youtube.com
 // @author          TalkLounge (https://github.com/TalkLounge)
 // @namespace       https://github.com/TalkLounge/cleanup-youtube-recommendations-search-playlist
@@ -19,12 +19,13 @@
         if (window.location.href.startsWith("https://www.youtube.com/playlist")) {
             document.querySelector('ytd-item-section-renderer[is-playlist-video-container]')?.remove(); // Recommended videos
             document.querySelector('ytd-item-section-renderer[is-playlist-shelf]')?.remove(); // Recommended playlists
+            document.querySelector('ytd-item-section-renderer:has(ytd-shelf-renderer)[page-subtype=playlist]')?.remove(); // Recommended playlists
             document.querySelector('ytd-continuation-item-renderer:nth-child(2)')?.remove(); // Loading circle
         }
 
         if (window.location.href.startsWith("https://www.youtube.com/results")) {
-            [...document.querySelectorAll("ytd-shelf-renderer")].forEach(item => item.remove()); // Sections: Popular videos from today, Users have also seen, New channels for you, For you, Already viewed
-            [...document.querySelectorAll("ytd-horizontal-card-list-renderer")].forEach(item => item.remove());
+            [...document.querySelectorAll("ytd-shelf-renderer")].forEach(item => item.remove()); // Sections: People also watched, For you, Previously watched, From related searches, Latest from, Popular videos from today, New channels for you
+            [...document.querySelectorAll("ytd-horizontal-card-list-renderer")].forEach(item => item.remove()); // Section: People also search for
         }
 
         if (window.location.href.startsWith("https://www.youtube.com/@") && (!window.location.pathname.substr(2).includes("/") || window.location.pathname.endsWith("/featured"))) {
